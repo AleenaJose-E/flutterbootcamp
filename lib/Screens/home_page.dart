cce7f4c4a48b157a74a62f048b04374e3162973e
Alee-boop <84243631+Alee-boop@users.noreply.github.com>
2021-06-08 21:47:48 +0530

    Third day

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -11,16 +11,33 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body : SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TopBar(title: 'Pinky Club', 
-            subtitle: 'Welcome Buddies !.....',
-             color: Colors.pinkAccent
-             ),
-             const SizedBox(height: 20),
-             Text('Photography',style: TextStyle(fontSize: 22,color: Colors.pink[300]),)
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TopBar(title: 'Pinky Club', 
+              subtitle: 'Welcome Buddies !.....',
+               color: Colors.pinkAccent
+               ),
+               const SizedBox(height: 20),
+               Text('Photography',style: TextStyle(fontSize: 22,color: Colors.pink[300]),),
+                const SizedBox(height: 20),
+               GridView.builder(
+                 padding: EdgeInsets.all(10),
+                 itemCount: 4,
+                 shrinkWrap: true,
+                 physics: const NeverScrollableScrollPhysics(),
+                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                   crossAxisSpacing: 10,
+                   mainAxisSpacing: 10,
+                   crossAxisCount: 2), 
+                 itemBuilder: (ctx, index) => Container(
+                  
+                   child: Image.network('https://images.unsplash.com/photo-1623014204234-f5c255b37b34?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80',fit: BoxFit.cover,),
+                 ),
+                 )
+            ],
+          ),
         ),
         
       ),
